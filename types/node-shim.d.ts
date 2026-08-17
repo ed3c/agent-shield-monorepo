fe1415e164497db557523eaa7d04e1cc2157459f
ed3c <mcnum01@gmail.com>
2026-08-18 01:45:01 +0800

    types: cover bounded PDF Bun/Node primitives

--- types/node-shim.d.ts
+++ types/node-shim.d.ts
@@ -4,6 +4,14 @@ declare const process: {
   exit(code?: number): never;
 };
 
+declare const Buffer: any;
+
+declare module "bun:test" {
+  export const describe: (...args: any[]) => any;
+  export const expect: (...args: any[]) => any;
+  export const test: (...args: any[]) => any;
+}
+
 declare module "node:child_process" {
   export const spawnSync: (...args: any[]) => any;
 }
@@ -27,4 +35,8 @@ declare module "node:os" {
 }
 declare module "node:crypto" {
   export const createHash: (...args: any[]) => any;
+}
+declare module "node:zlib" {
+  export const inflateSync: (...args: any[]) => any;
+  export const deflateSync: (...args: any[]) => any;
 }
